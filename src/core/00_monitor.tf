9603a735e889920dfa1aaf9ba47131579ffc4aee
diegoitaliait <diego.lagosmorales@pagopa.it>
2021-11-24 17:39:30 +0100

    core/monitoring: now are more independent

--- src/core/00_monitor.tf
+++ src/core/00_monitor.tf
@@ -7,3 +7,15 @@ data "azurerm_application_insights" "application_insights" {
   name                = format("%s-appinsights", local.project)
   resource_group_name = data.azurerm_resource_group.rg_monitor.name
 }
+
+# 🔭 monitor action groups
+
+data "azurerm_monitor_action_group" "slack" {
+  resource_group_name = local.monitor_rg
+  name                = local.monitor_action_group_slack_name
+}
+
+data "azurerm_monitor_action_group" "email" {
+  resource_group_name = local.monitor_rg
+  name                = local.monitor_action_group_email_name
+}
