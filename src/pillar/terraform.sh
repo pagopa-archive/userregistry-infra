45aa6377f587ddb928b32bd2417a99e17f142496
diegoitaliait <diego.lagosmorales@pagopa.it>
2021-11-26 16:27:47 +0100

    terraform.sh added compact warnings

--- src/pillar/terraform.sh
+++ src/pillar/terraform.sh
@@ -43,7 +43,7 @@ if echo "init plan apply refresh import output state taint destroy" | grep -w "$
     terraform init -reconfigure -backend-config="${BACKEND_CONFIG_PATH}"
 
     echo "[INFO] run tf with: ${ACTION} on ENV: ${ENV} and other: >${other}<"
-    terraform "${ACTION}" -var-file="../.env/${ENV}/terraform.tfvars" $other
+    terraform "${ACTION}" -var-file="../.env/${ENV}/terraform.tfvars" $other -compact-warnings
   fi
 else
     echo "[ERROR] ACTION not allowed."
