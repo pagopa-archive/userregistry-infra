aab17fe4ad8cab30383dbd27d6b54f033ea21bb9
diegoitaliait <diego.lagosmorales@pagopa.it>
2021-11-26 14:44:10 +0100

    minor fix

--- src/k8s/scripts/setup.sh
+++ src/k8s/scripts/setup.sh
@@ -1,42 +1,56 @@
 #!/usr/bin/env bash
+set -e
 
 #
 # Setup configuration relative to a given subscription
 # Subscription are defined in ./subscription
 # Usage:
-#  ./setup.sh ENV-SelfCare
+#  ./setup.sh <ENV>
 #
-#  ./setup.sh DEV-SelfCare
-#  ./setup.sh UAT-SelfCare
-#  ./setup.sh PROD-SelfCare
+#  ./setup.sh dev
+#  ./setup.sh uat
+#  ./setup.sh prod
 
-BASHDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-WORKDIR="${BASHDIR//scripts/}"
+SCRIPT_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CURRENT_DIRECTORY="$(basename "$SCRIPT_PATH")"
+ENV=$1
+# must be subscription in lower case
+subscription=""
 
-set -e
+#
+# 🏁 start shell init
+#
+if [ -z "$ENV" ]; then
+  echo "[ERROR] ENV should be: dev, uat or prod."
+  exit 0
+fi
 
-SUBSCRIPTION=$1
+# shellcheck source=/dev/null
+source "../../.env/$ENV/backend.ini"
 
-if [ -z "${SUBSCRIPTION}" ]; then
-    printf "\e[1;31mYou must provide a subscription as first argument.\n"
+if [ -z "${subscription}" ]; then
+    printf "[ERROR] \e[1;31mYou must provide a subscription.\n"
     exit 1
 fi
 
-if [ ! -d "${WORKDIR}/subscriptions/${SUBSCRIPTION}" ]; then
-    printf "\e[1;31mYou must provide a subscription for which a variable file is defined. You provided: '%s'.\n" "${SUBSCRIPTION}" > /dev/stderr
-    exit 1
-fi
+echo "[INFO] This is the current directory: ${CURRENT_DIRECTORY}"
 
-az account set -s "${SUBSCRIPTION}"
+echo "[INFO] Subscription: ${subscription}"
+az account set -s "${subscription}"
 
-aks_name=$(az aks list -o tsv --query "[?contains(name,'aks')].{Name:name}")
-aks_resource_group_name=$(az aks list -o tsv --query "[?contains(name,'aks')].{Name:resourceGroup}")
-aks_private_fqdn=$(az aks list -o tsv --query "[?contains(name,'aks')].{Name:privateFqdn}")
+#
+# LOAD VARIABLES
+#
+aks_name_from_cli=$(az aks list -o tsv --query "[?contains(name,'aks')].{Name:name}")
+echo "[INFO] aks_name_from_cli: ${aks_name_from_cli}"
+aks_resource_group_name_from_cli=$(az aks list -o tsv --query "[?contains(name,'aks')].{Name:resourceGroup}")
+echo "[INFO] aks_resource_group_name_from_cli: ${aks_resource_group_name_from_cli}"
 
-# in widows, even if using cygwin, these variables will contain a landing \r character
-aks_name=${aks_name//[$'\r']}
-aks_resource_group_name=${aks_resource_group_name//[$'\r']}
-aks_private_fqdn=${aks_private_fqdn//[$'\r']}
+# ⚠️ in widows, even if using cygwin, these variables will contain a landing \r character
+aks_name=${aks_name_from_cli//[$'\r']}
+echo "[INFO] aks_name: ${aks_name}"
+aks_resource_group_name=${aks_resource_group_name_from_cli//[$'\r']}
+echo "[INFO] aks_resource_group_name: ${aks_resource_group_name}"
 
 # if using cygwin, we have to transcode the WORKDIR
 HOME_DIR=$HOME
@@ -45,11 +59,12 @@ if [[ $HOME_DIR == /cygdrive/* ]]; then
   HOME_DIR=${HOME_DIR//\\//}
 fi
 
+#
+# 🖥 start script
+#
 rm -rf "${HOME}/.kube/config-${aks_name}"
-az aks get-credentials -g "${aks_resource_group_name}" -n "${aks_name}" --subscription "${SUBSCRIPTION}" --file "~/.kube/config-${aks_name}"
-az aks get-credentials -g "${aks_resource_group_name}" -n "${aks_name}" --subscription "${SUBSCRIPTION}" --overwrite-existing
-echo "aks_private_fqdn=${aks_private_fqdn}" >> "${WORKDIR}/subscriptions/${SUBSCRIPTION}/.bastianhost.ini"
-echo "kube_config_path=${HOME_DIR}/.kube/config-${aks_name}" >> "${WORKDIR}/subscriptions/${SUBSCRIPTION}/.bastianhost.ini"
+az aks get-credentials -g "${aks_resource_group_name}" -n "${aks_name}" --subscription "${subscription}" --file "~/.kube/config-${aks_name}"
+az aks get-credentials -g "${aks_resource_group_name}" -n "${aks_name}" --subscription "${subscription}" --overwrite-existing
 
 # with AAD auth enabled we need to authenticate the machine on the first setup
 echo "Follow Microsoft sign in steps. kubectl get pods command will fail but it's the expected behavior"
