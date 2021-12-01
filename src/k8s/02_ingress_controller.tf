f5b4c5fd06c443152a1c8e8600428028e948194a
diegoitaliait <diego.lagosmorales@pagopa.it>
2021-12-01 12:42:21 +0100

    completed helm ingress controller installation

--- src/k8s/02_ingress_controller.tf
+++ src/k8s/02_ingress_controller.tf
@@ -1,34 +1,43 @@
 # from Microsoft docs https://docs.microsoft.com/it-it/azure/aks/ingress-internal-ip
-resource "helm_release" "ingress" {
-  name       = "nginx-ingress"
+module "nginx_ingress" {
+
+  source  = "terraform-module/release/helm"
+  version = "2.7.0"
+
+  namespace  = kubernetes_namespace.usrreg.metadata[0].name
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = var.nginx_helm_version
-  namespace  = kubernetes_namespace.ingress.metadata[0].name
+  app = {
+    name          = "ingress-nginx"
+    version       = "3.33.0"
+    chart         = "ingress-nginx"
+    force_update  = true
+    wait          = false
+    recreate_pods = false
+    deploy        = 1
+  }
 
   values = [
     "${templatefile("${path.module}/ingress/loadbalancer.yaml.tpl", { load_balancer_ip = var.ingress_load_balancer_ip })}"
   ]
 
-  set {
-    name  = "controller.replicaCount"
-    value = var.ingress_replica_count
-  }
-
-  set {
-    name  = "controller.nodeSelector.beta\\.kubernetes\\.io/os"
-    value = "linux"
-  }
-
-  set {
-    name  = "defaultBackend.nodeSelector.beta\\.kubernetes\\.io/os"
-    value = "linux"
-  }
-
-  set {
-    name  = "controller.admissionWebhooks.patch.nodeSelector.beta\\.kubernetes\\.io/os"
-    value = "linux"
-  }
+  set = [
+    {
+      name  = "controller.replicaCount"
+      value = var.ingress_replica_count
+    },
+    {
+      name  = "controller.nodeSelector.beta\\.kubernetes\\.io/os"
+      value = "linux"
+    },
+    {
+      name  = "defaultBackend.nodeSelector.beta\\.kubernetes\\.io/os"
+      value = "linux"
+    },
+    {
+      name  = "controller.admissionWebhooks.patch.nodeSelector.beta\\.kubernetes\\.io/os"
+      value = "linux"
+    }
+  ]
 
   depends_on = [
     kubernetes_namespace.ingress
